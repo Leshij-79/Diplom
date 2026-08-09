12b86cfc70f152e5525933f9e75ba30803508792
Leshij-79 <freeman8901@yandex.ru>
2026-08-09 22:12:43 +0500

    Доработка замечаний

--- config/settings.py
+++ config/settings.py
@@ -12,7 +12,8 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+# ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS")
+ALLOWED_HOSTS = ["*"]
 
 CSRF_TRUSTED_ORIGINS = os.getenv("CSRF_TRUSTED_ORIGINS")
 
